560ee259e1f595b4de5aa04980d7c9f59453424f
Apisit Tananchai <apisit.tananchai@emc.com>
2019-01-18 01:07:32 +0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Apisit Tananchai on 18/1/2562 BE.
+//  Copyright © 2562 Apisit Tananchai. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
